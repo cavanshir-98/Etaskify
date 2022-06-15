7a583ccd21cab0309b1fccf65852925af57544c9
cavanshir-98 <cavansir.asad@gmail.com>
2022-06-15 14:58:22 +0400

    added firt

---
+++ src/test/java/webApplication/Etaskify/EtaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package webApplication.Etaskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtaskifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
